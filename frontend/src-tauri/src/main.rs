4798b647c8d9e6a9fd918d83fd011496c1629495
Thomas Schaller <me@torkleyy.com>
2021-11-23 15:39:30 +0100

    fix(frontend): remove unneeded models dependency

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -3,25 +3,9 @@
   windows_subsystem = "windows"
 )]
 
-use models::{Author, PagedResponseData};
-
-const URL: &'static str = "http://localhost:8000/v1";
-
-#[tauri::command]
-async fn authors(start: u64, limit: u64) -> PagedResponseData<Author> {
-  async {
-    reqwest::get(format!("{}/authors?start={}&limit={}", URL, start, limit))
-      .await?
-      .json()
-      .await
-  }
-  .await
-  .unwrap()
-}
-
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![authors])
+    .invoke_handler(tauri::generate_handler![])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
